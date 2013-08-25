32e1de76910af8acd7bc8c5d4eef38b2805b172c
Sam Dodrill <niichan@ponychat.net>
2013-08-25 15:01:56 -0700

    ii macros updated

--- .zsh/ii-commands.zsh
+++ .zsh/ii-commands.zsh
@@ -1,6 +1,15 @@
+#!/bin/zsh
+
+#Aliases that are useful when used with ii
+
+alias iicmd="echo $* >> in"
 alias iiroot="cd ~/irc"
-alias iijoin="echo \"/j $*\" >> in"
-alias iiread="cat $1/out"
-alias iitail="iiread $1 | tail -n 10"
-alias iipart="echo \"/part $1\" >> in"
-alias iisay="echo $* >> in"
+alias iijoin="iicmd /j $*"
+#alias iiread="cat $1/out"
+#alias iitail="iiread $1 | tail -n 10"
+alias iipart="iicmd /l $*"
+alias iisay="iicmd"
+alias iiaway="iicmd /a $*"
+alias iitopic="iicmd /t $*"
+alias iinick="iicmd /n $1"
+
