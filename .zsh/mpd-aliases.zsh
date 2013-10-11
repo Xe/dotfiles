e00e12558b73853fae5946470e816fdc18d14988
Sam Dodrill <shadow.h511@gmail.com>
2013-10-10 19:10:04 -0700

    Updated MPD aliases

--- .zsh/mpd-aliases.zsh
+++ .zsh/mpd-aliases.zsh
@@ -1,5 +1,10 @@
 # Aliases for mpd
 
+MPD_REMOTE_HOST="sparkle.yolo-swag.com"
+
 alias play="mpc play > /dev/null"
 alias stop="mpc stop > /dev/null"
+alias cursong="mpc | head -n 1"
+alias next="mpc next -h $MPD_REMOTE_HOST > /dev/null; sleep 1 ; cursong"
+alias prev="mpc prev -h $MPD_REMOTE_HOST > /dev/null; sleep 1 ; cursong"
 alias np="mpc status"
