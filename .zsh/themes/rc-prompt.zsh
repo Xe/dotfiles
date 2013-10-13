02ddcebe19eb9c5c314f30473ae534f4ca33c31f
Sam Dodrill <shadow.h511@gmail.com>
2013-10-13 09:29:10 -0700

    Fix prompt for modified oh my zsh library files

--- .zsh/themes/rc-prompt.zsh
+++ .zsh/themes/rc-prompt.zsh
@@ -3,6 +3,6 @@
 PROMPT='$(git_prompt_info); %{$reset_color%}'
 
 ZSH_THEME_GIT_PROMPT_PREFIX=""
-ZSH_THEME_GIT_PROMPT_SUFFIX=" "
+ZSH_THEME_GIT_PROMPT_SUFFIX=""
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_bold[yellow]%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}"
