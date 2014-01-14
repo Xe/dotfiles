c8b2486406088863fc28bc9d3f872dcafa684b70
Sam Dodrill <shadow.h511@gmail.com>
2014-01-14 09:08:29 -0500

    cheat-uclibc: don't cheat uclibc unless we're on Alpine

--- .zsh/cheat-uclibc.zsh
+++ .zsh/cheat-uclibc.zsh
@@ -1,6 +1,7 @@
 #cheating uclibc to do UTF-8
-LC_ALL='en_US.UTF-8'
-LANG='en_US.UTF-8'
-LC_CTYPE=C
-CHARSET=UTF-8
-
+if [ -x /sbin/apk ] ; then
+	export LC_ALL='en_US.UTF-8'
+	export LANG='en_US.UTF-8'
+	export LC_CTYPE=C
+	export CHARSET=UTF-8
+fi
