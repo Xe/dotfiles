2e6122a2a2dea96f338f9b0f9648589bba4cb883
Sam Dodrill <shadow.h511@gmail.com>
2014-06-02 10:51:21 -0700

    zsh/theme: remove unicode

--- .zsh/themes/niichan.zsh
+++ .zsh/themes/niichan.zsh
@@ -2,7 +2,7 @@
 
 PROMPT='%m %{$fg_bold[magenta]%}%c$(git_prompt_info) %{$fg_bold[green]%}>%{$fg_bold[magenta]%}%{$fg_bold[magenta]%} % %{$reset_color%}'
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" (:%{$fg_bold[green]%}"
+ZSH_THEME_GIT_PROMPT_PREFIX=" (%{$fg_bold[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_bold[magenta]%}) %{$fg[yellow]%}X%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[magenta]%}) %{$fg[green]%}O%{$reset_color%}"
