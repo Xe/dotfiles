150704a3347374eee7ada5ef83e39c32c1b51b25
Sam Dodrill <xena@yolo-swag.com>
2014-06-08 13:54:05 -0700

    zsh/dtach: make dtach shit better

--- .zsh/dtach.zsh
+++ .zsh/dtach.zsh
@@ -1,2 +1,10 @@
 alias wm="dtach -A /tmp/dvtm -r winch dvtm"
-alias session="dtach -A /tmp/dvtm-$1 -r winch dvtm"
+alias session="dtach -A /tmp/dvtm.$1 -r winch /usr/local/bin/dvtm"
+
+function detach {
+if [ -S /tmp/$1.dtach ]; then
+	dtach -a /tmp/$1.dtach -r winch
+else
+	dtach -c /tmp/$1.dtach -r winch $1
+fi
+}
