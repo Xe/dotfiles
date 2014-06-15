f916e5b90c57c6b3b964963d5baf4b264211fcdb
Sam Dodrill <xena@yolo-swag.com>
2014-06-15 16:06:36 -0700

    add anonymous ubuntu alias

--- .zsh/docker.zsh
+++ .zsh/docker.zsh
@@ -1,3 +1,7 @@
 alias dockercleancontainers="docker ps -a -notrunc| grep 'Exit' | awk '{print \$1}' | xargs -L 1 -r docker rm"
 alias dockercleanimages="docker images -a -notrunc | grep none | awk '{print \$3}' | xargs -L 1 -r docker rmi"
 alias dockerclean="dockercleancontainers && dockercleanimages"
+
+function anonubuntu {
+	docker run -it /bin/bash
+}
