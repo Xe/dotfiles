39fff2e034e10d1711b6f6624f64ed25560c858d
Sam Dodrill <xena@yolo-swag.com>
2014-07-17 18:39:28 -0700

    Fix dokku alias

--- .zsh/dokku.zsh
+++ .zsh/dokku.zsh
@@ -1,6 +1,13 @@
 function dokku {
-	if [[ "$1" -eq "create" ]] then
-		git remote add dokku dokku@dokku.xeserv.us:${PWD##*/}
+	if [ "$1" = "create" ]
+	then
+		if git remote add dokku dokku@dokku.xeserv.us:${PWD##*/}
+		then
+			echo "-----> Dokku remote added at xeserv.us"
+		else
+			echo "!      Dokku remote not added! Do you already have a dokku remote?"
+			return
+		fi
 		git push dokku master
 	else
 		ssh dokku@dokku.xeserv.us $*
