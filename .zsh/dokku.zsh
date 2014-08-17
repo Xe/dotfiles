5c16722386caf87156e9385258a794e2be59b88a
Sam Dodrill <xena@yolo-swag.com>
2014-08-16 19:02:52 -0700

    Improve dokku script

--- .zsh/dokku.zsh
+++ .zsh/dokku.zsh
@@ -1,17 +1,32 @@
-function dokku {
-	if [ "$1" = "create" ]
-	then
-		appname=$(echo "print(elfs.GenName())" | lua -l elfs)
-		if git remote add dokku dokku@dokku.xeserv.us:$appname
+if [ -z "$DOKKU_CONTROLLER" ]
+then
+	function dokku {
+		appname=$(git remote -v 2>/dev/null | grep dokku | head -n 1 | cut -f1 -d' ' | cut -f2 -d':' 2>/dev/null)
+		if [ "$?" != "0" ]
 		then
-			echo "-----> Dokku remote added at xeserv.us"
-			echo "-----> Application name is $appname"
-		else
-			echo "!      Dokku remote not added! Do you already have a dokku remote?"
-			return
+			donotshift="YES"
 		fi
-		git push dokku master
-	else
-		ssh dokku@dokku.xeserv.us $*
-	fi
-}
+		if [ "$1" = "create" ]
+		then
+			appname=$(echo "print(elfs.GenName())" | lua -l elfs)
+			if git remote add dokku dokku@$DOKKU_CONTROLLER:$appname
+			then
+				echo "-----> Dokku remote added at $DOKKU_CONTROLLER"
+				echo "-----> Application name is $appname"
+			else
+				echo "!      Dokku remote not added! Do you already have a dokku remote?"
+				return
+			fi
+			git push dokku master
+		else
+			if [ -z "$donotshift" ]
+			then
+				ssh dokku@$DOKKU_CONTROLLER $*
+				exit
+			fi
+			verb=$1
+			shift
+			ssh dokku@$DOKKU_CONTROLLER "$verb" "$appname" $@
+		fi
+	}
+fi
