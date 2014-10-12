a7cc5d45b42e745b6e3e374256c2bbaadede38cb
Sam Dodrill <xena@yolo-swag.com>
2014-10-12 14:07:02 -0700

    hexchat: get certificates

--- cloud/dockerfiles/comms/hexchat/run.sh
+++ cloud/dockerfiles/comms/hexchat/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix xena/hexchat
+docker run -dit -v /home/xena --name hexchat-data xena/hexchat /bin/bash
+docker run -ti -d --name hexchat -e DISPLAY=$DISPLAY --volumes-from hexchat-data -v /tmp/.X11-unix:/tmp/.X11-unix xena/hexchat
