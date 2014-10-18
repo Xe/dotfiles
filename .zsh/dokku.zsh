b725e2f61909489c37eb797ea70fb0161827d5e7
Sam Dodrill <xena@yolo-swag.com>
2014-10-17 20:16:33 -0700

    dokku: what was I smoking

--- .zsh/dokku.zsh
+++ .zsh/dokku.zsh
@@ -1,28 +1,26 @@
-if [ -z "$DOKKU_CONTROLLER" ]
+function dokku {
+if [ "$1" = "create" ]
 then
-	function dokku {
-	if [ "$1" = "create" ]
+	appname=$(echo "print(elfs.GenName())" | lua -l elfs)
+	if git remote add dokku dokku@$DOKKU_CONTROLLER:$appname
 	then
-		appname=$(echo "print(elfs.GenName())" | lua -l elfs)
-		if git remote add dokku dokku@$DOKKU_CONTROLLER:$appname
-		then
-			echo "-----> Dokku remote added at $DOKKU_CONTROLLER"
-			echo "-----> Application name is $appname"
-		else
-			echo "!      Dokku remote not added! Do you already have a dokku remote?"
-			return
-		fi
-		git push dokku master
+		echo "-----> Dokku remote added at $DOKKU_CONTROLLER"
+		echo "-----> Application name is $appname"
 	else
-		appname=$(git remote -v 2>/dev/null | grep dokku | head -n 1 | cut -f1 -d' ' | cut -f2 -d':' 2>/dev/null)
-		if [ "$?" != "0" ]
-		then
-			ssh dokku@$DOKKU_CONTROLLER $*
-			exit
-		fi
-		verb=$1
-		shift
-		ssh dokku@$DOKKU_CONTROLLER "$verb" "$appname" $@
+		echo "!      Dokku remote not added! Do you already have a dokku remote?"
+		return
 	fi
-}
+	git push dokku master
+else
+	appname=$(git remote -v 2>/dev/null | grep dokku | head -n 1 | cut -f1 -d' ' | cut -f2 -d':' 2>/dev/null)
+	if [ "$?" != "0" ]
+	then
+		ssh dokku@$DOKKU_CONTROLLER $*
+		exit
+	fi
+	verb=$1
+	shift
+	ssh dokku@$DOKKU_CONTROLLER "$verb" "$appname" $@
 fi
+}
+
