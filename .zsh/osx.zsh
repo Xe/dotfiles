5780f4da6b457a3c7d387a1c3beaccafc44b50f1
Sam Dodrill <xena@yolo-swag.com>
2014-11-07 14:47:24 -0800

    make OSX stuff more smart

--- .zsh/osx.zsh
+++ .zsh/osx.zsh
@@ -1,2 +1,9 @@
 # Don't use doored code
-export HOMEBREW_BUILD_FROM_SOURCE=1
+
+if [ "$(uname)" = "Darwin" ]; then
+	export HOMEBREW_BUILD_FROM_SOURCE=1
+
+	export DOCKER_HOST=tcp://192.168.59.103:2376
+	export DOCKER_CERT_PATH=/Users/xena/.boot2docker/certs/boot2docker-vm
+	export DOCKER_TLS_VERIFY=1
+fi
