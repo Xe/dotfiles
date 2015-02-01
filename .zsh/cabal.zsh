56a4a49ef4e358e55c2b8d41ad3db4e40579d142
Sam Dodrill <xena@yolo-swag.com>
2015-02-01 10:47:48 -0800

    add sandbox to cabal.zsh

--- .zsh/cabal.zsh
+++ .zsh/cabal.zsh
@@ -1,1 +1,14 @@
 export PATH=$PATH:~/.cabal/bin
+
+function cabal_sandbox_info() {
+cabal_files=(*.cabal(N))
+if [ $#cabal_files -gt 0 ]; then
+	if [ -f cabal.sandbox.config ]; then
+		echo "%{$fg[green]%}sandboxed%{$reset_color%}"
+	else
+		echo "%{$fg[red]%}not sandboxed%{$reset_color%}"
+	fi
+fi
+}
+
+RPROMPT="\$(cabal_sandbox_info) $RPROMPT"
