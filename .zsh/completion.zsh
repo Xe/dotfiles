d89d0606b828a946e12473bcb9a302003a799899
Christine Dodrill <xena@yolo-swag.com>
2015-03-11 11:20:42 -0700

    completion: fuzzy matching :D

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -5,3 +5,11 @@ setopt completeinword
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 autoload select-word-style
 select-word-style shell
+
+# Menu!
+zstyle ':completion:*' menu select
+
+# Fuzzy matching
+zstyle ':completion:::::' completer _complete _approximate
+# limited to 3 errors per character typed
+zstyle ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX+$#SUFFIX)/3 )) numeric )'
