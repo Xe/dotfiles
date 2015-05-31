7990165131046cf76fe02f274cab05f48f5c48b5
Christine Dodrill <xena@yolo-swag.com>
2015-05-31 16:21:25 -0700

    fix irssi command

--- .zsh/docker.zsh
+++ .zsh/docker.zsh
@@ -6,11 +6,10 @@ function anonubuntu {
 	docker run --rm -it ubuntu:trusty /bin/bash
 }
 
-function godevel {
-	docker run --rm -it -v /home/xena/go:/home/xena/go xena/base-devel /bin/zsh
+function anonirssi {
+	docker run --rm -ite TERM=$TERM irssi
 }
 
 if [ "$(uname)" = "Darwin" ]; then
 	export DOCKER_HOST=tcp://192.168.59.103:2375
-	alias vim='DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=/usr/local/Cellar/python/2.7.8_2/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib vim'
 fi
