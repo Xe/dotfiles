6b1a06111b4a4ce77a23385ee332f69f07f11d15
Christine Dodrill <xena@yolo-swag.com>
2015-06-16 21:24:37 -0700

    Passthru kernel modules for rancheros"

--- rancher-run.sh
+++ rancher-run.sh
@@ -7,6 +7,7 @@ docker run \
 	-v /home/rancher/shell:/home/xena/var \
 	-v /home/rancher/shell/bin:/home/xena/bin \
 	-v /var/run/docker.sock \
+	-v /lib/modules/3.19.3-rancher/kernel:/lib/modules/3.19.3-rancher/kernel:ro \
 	--net=host \
 	--privileged \
 	--restart=always \
