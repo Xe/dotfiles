87c5f9416570c8dce6ced94b3fe30e02e5fc1cbf
Christine Dodrill <xena@yolo-swag.com>
2015-10-06 11:40:28 -0700

    Add xe-weechat layer

---
+++ spacemacs/xe-weechat/packages.el
@@ -0,0 +1,15 @@
+;;; weechat mode for xena's spacemacs
+
+(setq xe-weechat-packages
+      '(weechat))
+
+(defun xe-weechat/init-weechat ()
+  (use-package weechat)
+  (use-package weechat-tracking)
+  (use-package weechat-notifications)
+  (use-package weechat-modules)
+  (setq weechat-color-list '(unspecified "black" "dim gray" "dark red" "red"
+                                         "dark green" "green" "brown"
+                                         "orange" "dark blue" "blue"
+                                         "dark magenta" "magenta" "dark cyan"
+                                          "royal blue" "dark gray" "gray")))
