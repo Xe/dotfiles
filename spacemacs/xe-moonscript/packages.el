768034c876c3029c1c017e1412eed6e941db5622
Christine Dodrill <xena@yolo-swag.com>
2015-10-13 19:04:33 -0700

    fix moonscript mode

--- spacemacs/xe-moonscript/packages.el
+++ spacemacs/xe-moonscript/packages.el
@@ -1,10 +1,8 @@
 ;;; moonscript mode for xena's spacemacs
 
 (setq xe-moonscript-packages
-      '((moonscript-mode :location (recipe
-                                    :fetcher github
-                                    :repo "k2052/moonscript-mode"))))
+      '((moonscript-mode :location local)))
 
 (defun xe-moonscript/init-moonscript-mode ()
-  (use-package moonscript-mode
-    :defer t))
+  (use-package moonscript-mode)
+  (use-package moonscriptrepl-mode))
