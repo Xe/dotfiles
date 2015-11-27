ef0e0428c871ac1761168a9496bdeba8eb456be7
Christine Dodrill <xena@yolo-swag.com>
2015-11-27 12:33:51 -0800

    use melpa sr-speedbar

--- spacemacs/xe-sr-speedbar/packages.el
+++ spacemacs/xe-sr-speedbar/packages.el
@@ -1,7 +1,7 @@
 ;; sr-speedbar
 
 (setq xe-sr-speedbar-packages
-    '((sr-speedbar :location local)))
+    '((sr-speedbar))
 
 (defun xe-sr-speedbar/init-sr-speedbar ()
   (use-package sr-speedbar))
