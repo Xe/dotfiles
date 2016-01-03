70f07349d1d4f58a4a2c34f079fea6ba7abfbcf3
Christine Dodrill <xena@yolo-swag.com>
2016-01-02 18:13:08 -0800

    Update dotfiles ansible

--- ansible/scripts/common/install_go.sh
+++ ansible/scripts/common/install_go.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 cd /usr/local
-wget https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz
-tar xf go1.5.linux-amd64.tar.gz
-rm go1.5.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.5.2.linux-amd64.tar.gz
+tar xf go1.5.2.linux-amd64.tar.gz
+rm go1.5.2.linux-amd64.tar.gz
