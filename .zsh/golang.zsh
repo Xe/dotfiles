0d6ac309e2e07cd80d02ed8aea4b037427b4e377
Christine Dodrill <xena@yolo-swag.com>
2016-01-28 13:51:59 -0800

    Add luarocks path

--- .zsh/golang.zsh
+++ .zsh/golang.zsh
@@ -7,3 +7,4 @@ fi
 export PATH=$PATH:/usr/local/go/bin:/home/xena/prefix/go/bin
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
+export GO15VENDOREXPERIMENT=1
--- .zsh/luarocks.zsh
+++ .zsh/luarocks.zsh
@@ -7,3 +7,5 @@ function mri {
 		moonrocks install $package --local
 	done
 }
+
+eval "$(luarocks path)"
