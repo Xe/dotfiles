fcf6ac376aa01b4dc12529224c48192490f495f8
Christine Dodrill <xena@yolo-swag.com>
2016-03-17 12:01:18 -0700

    Update elnode package list

--- spacemacs/xe-elnode/packages.el
+++ spacemacs/xe-elnode/packages.el
@@ -1,7 +1,17 @@
 ;;; elnode mode for xena's spacemacs
 
 (setq xe-elnode-packages
-      '(elnode))
+      '(elnode
+        (mustache :location (recipe
+                             :fetcher github
+                             :repo "Wilfred/mustache.el"))
+        ht))
 
 (defun xe-elnode/init-elnode ()
   (use-package elnode))
+
+(defun xe-elnode/init-mustache ()
+  (use-package mustache))
+
+(defun xe-elnode/init-ht ()
+  (use-package ht))
