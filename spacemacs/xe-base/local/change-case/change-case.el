0cadca41f4bea74af5c49f5ad5dfc76b8135a1a4
Cadey Dodrill <me@christine.website>
2016-04-13 16:44:07 -0700

    Add automatic camelcasing functions

---
+++ spacemacs/xe-base/local/change-case/change-case.el
@@ -0,0 +1,55 @@
+(defun camelcase-region (start end)
+  "Changes region from snake_case to camelCase"
+  (interactive "r")
+  (save-restriction (narrow-to-region start end)
+                    (goto-char (point-min))
+                    (while (re-search-forward "_\\(.\\)" nil t)
+                      (replace-match (upcase (match-string 1))))))
+
+(defun camelcase-word-or-region ()
+  "Changes word or region from snake_case to camelCase"
+  (interactive)
+  (let (pos1 pos2 bds)
+    (if (and transient-mark-mode mark-active)
+        (setq pos1 (region-beginning) pos2 (region-end))
+      (progn
+        (setq bds (bounds-of-thing-at-point 'symbol))
+        (setq pos1 (car bds) pos2 (cdr bds))))
+    (camelcase-region pos1 pos2)))
+
+(defun split-name (s)
+  (split-string
+   (let ((case-fold-search nil))
+     (downcase
+      (replace-regexp-in-string "\\([a-z]\\)\\([A-Z]\\)" "\\1 \\2" s)))
+   "[^A-Za-z0-9]+"))
+
+(defun underscore-string (s)
+  (mapconcat
+   'downcase
+   (split-name s) "_"))
+
+(defun snakecase-region (begin end)
+  "Convert the given region from camelCase to snake_case"
+  (interactive "r")
+  (let* ((word (buffer-substring begin end))
+         (underscored (underscore-string word)))
+    (save-excursion
+      (widen) ; break out of the subregion so we can fix every usage of the function
+      (replace-string word underscored nil (point-min) (point-max)))))
+
+(defun snakecase-word-or-region ()
+  "Changes word or region from camelCase to snake_case"
+  (interactive)
+  (let (pos1 pos2 bds)
+    (if (and transient-mark-mode mark-active)
+        (setq pos1 (region-beginning) pos2 (region-end))
+      (progn
+        (setq bds (bounds-of-thing-at-point 'symbol))
+        (setq pos1 (car bds) pos2 (cdr bds))))
+    (snakecase-region pos1 pos2)))
+
+(global-set-key (kbd "C-c C--") 'camelcase-word-or-region)
+(global-set-key (kbd "C-c C-_") 'snakecase-word-or-region)
+
+(provide 'change-case)
---
+++ spacemacs/xe-base/packages.el
@@ -0,0 +1,5 @@
+(setq xe-base-packages
+      '((change-case :location local)))
+
+(defun xe-base/init-change-case ()
+  (use-package change-case))
